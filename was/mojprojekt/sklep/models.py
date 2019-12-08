ca144f201562e63e01af58ee3500781a7ee0c2d9
Lucas3wade <lukasz.konopko3@gmai.com>
2019-12-08 20:56:59 +0100

    Finished foruth homework

--- was/mojprojekt/sklep/models.py
+++ was/mojprojekt/sklep/models.py
@@ -43,3 +43,6 @@ class OrderedProducts(models.Model):
 class Complaint(models.Model):
     name = models.CharField(max_length=100)
     message = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.message[0:50]
